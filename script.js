c7f03008f65de4042bc9e632319c335a9d1bab4b
rodelul <98654686+rodelul@users.noreply.github.com>
2024-07-31 17:04:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => { 
+    new Splide( '.splide', {
+        type   : 'loop',
+        perPage: 1,
+        arrows: false,
+      } ).mount();
+});  
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
